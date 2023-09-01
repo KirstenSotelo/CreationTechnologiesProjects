05efbe32d3136e98402aa34739c2d865b1b41026
KirstenSotelo <143845168+KirstenSotelo@users.noreply.github.com>
2023-09-01 17:07:03 -0400

    Added 1.1

---
+++ RevisionCompareTool/RevisionCompareToolV1.1/RevisionCompareToolVersion1.1.py
@@ -0,0 +1,273 @@
+import os
+import sys
+from datetime import datetime
+from os import path
+
+def printBanner():
+    print("----------------------------------------------------------------")
+    print("   ██████╗ ███████╗██╗   ██╗██╗███████╗██╗ ██████╗ ███╗   ██╗   ") 
+    print("   ██╔══██╗██╔════╝██║   ██║██║██╔════╝██║██╔═══██╗████╗  ██║   ")
+    print("   ██████╔╝█████╗  ██║   ██║██║███████╗██║██║   ██║██╔██╗ ██║   ")
+    print("   ██╔══██╗██╔══╝  ╚██╗ ██╔╝██║╚════██║██║██║   ██║██║╚██╗██║   ")
+    print("   ██║  ██║███████╗ ╚████╔╝ ██║███████║██║╚██████╔╝██║ ╚████║   ")
+    print("   ╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚═╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ")
+    print("   ██████╗ ██████╗ ███╗   ███╗██████╗  █████╗ ██████╗ ███████╗  ")
+    print("  ██╔════╝██╔═══██╗████╗ ████║██╔══██╗██╔══██╗██╔══██╗██╔════╝  ")
+    print("  ██║     ██║   ██║██╔████╔██║██████╔╝███████║██████╔╝█████╗    ")
+    print("  ██║     ██║   ██║██║╚██╔╝██║██╔═══╝ ██╔══██║██╔══██╗██╔══╝    ")
+    print("  ╚██████╗╚██████╔╝██║ ╚═╝ ██║██║     ██║  ██║██║  ██║███████╗  ")
+    print("   ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝  ")
+    print("   ████████╗ ██████╗  ██████╗ ██╗                               ")
+    print("   ╚══██╔══╝██╔═══██╗██╔═══██╗██║                               ")
+    print("      ██║   ██║   ██║██║   ██║██║                               ")
+    print("      ██║   ██║   ██║██║   ██║██║                               ")
+    print("      ██║   ╚██████╔╝╚██████╔╝███████╗             Version 1.1  ")
+    print("      ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝      by: Kirsten Sotelo  ")
+    print("----------------------------------------------------------------")
+    print()
+    print()
+
+def printDict(dict):
+    tempString = "\nREF\tIPN\n"
+    for key in dict:
+        tempString += str(key) + "\t" + str(dict[key]) + "\n"
+    return tempString
+
+class refAndIpn:
+
+    # method for reading the file and collecting the Ref and Ipn
+    def collectRefAndIpn(file_path):
+        REFandIPNDict = {}
+
+    # --------- START OF READING LINES ---------------------
+        # Checking if reading the file path is valid
+        if(os.path.isfile(file_path)):
+
+            #if valid, open the filepath and set to read. 'while True' line is for "while this file has a next element, else, break"
+            file = open(file_path, 'r')
+            while True:
+
+                # Checking if the line exists, else, break from loop
+                line = file.readline()
+                if not line:
+                    break
+                
+                # Checking if the line is only empty spaces(for whatever reason), then ignore this line
+                if line.isspace():
+                    continue
+                
+                # split the line, and get rid of any empty strings and new line strings
+                splitted = line.split('\t')
+                while ('') in splitted:
+                    splitted.remove('')
+                while ('\n') in splitted:
+                    splitted.remove('\n')
+
+                # after organizing the line, check if the first 3 characters are equal to "GFT", if true, then ignore the whole line
+                """ if len(splitted) > 0:
+                    gftCheck = splitted[0][:3]
+                    if gftCheck == "GFT" or gftCheck == "GFB":
+                        continue """
+                    
+
+                # defining temporary variables
+                # X,Y,ROT,REST is not used for this program
+                ref = ""
+                ipn = 0
+                x = 0
+                y = 0
+                rot = 0
+                rest = ""
+
+                # the first 5 elements of splitted will always be the same format: str, int, int, int, str
+                # everything after that (rest) is unimportant info and varies in length
+                # use len(splitted), and assigning the first 5 elements accordingly, but if the count is greater than 5, just add it to the 'rest' variable
+                for i in range(len(splitted)):
+                    if i == 0:
+                        ref = str(splitted[i])
+                    elif i == 1:
+                        ipn = str(splitted[i])
+                    elif i == 2:
+                        x = int(splitted[i])
+                    elif i == 3:
+                        y = int(splitted[i])
+                    elif i == 4:
+                        rot = str(splitted[i])
+                    elif i >= 5:
+                        rest += str(splitted[i]) + " "
+
+                # create a refAndIpn object from these assigned variables AFTER the for loop above, and add it to the REFandIPNDict list if it has a valid ref and ipn.
+                #tempREFandIPN = refAndIpn(ref, ipn)
+
+                if ref == "" and x == 0 and y == 0 and rot == 0 and rest == "":
+                    continue
+                elif len(rest) <= 3:
+                    continue
+                else:
+                    #REFandIPNDict.update(ref = ipn)
+                    REFandIPNDict[ref] = ipn
+
+                # ---------- END OF READING LINES AND CREATING OBJECTS ---------------------------
+
+            # sort the dict by IPN first, then REF  (value then key)(not exactly sure how this works, but it works)
+            REFandIPNDict = {val[0] : val[1] for val in sorted(REFandIPNDict.items(), key = lambda x: (x[0], x[1]))}
+            
+            return REFandIPNDict
+        else:
+            raise Exception("This file is not compatible or does not exist")
+
+    # method for comparing this object with other object (able to use ==)
+    def __eq__(self, other): 
+        if not isinstance(other, refAndIpn):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.REF == other.REF and self.IPN == other.IPN
+    
+    def checkSame(self, other):
+        thisDict = self
+        otherDict = other
+        if thisDict == otherDict:
+            return True
+
+    def unchangedInNext(self, other):
+        thisDict = self
+        otherDict = other
+
+         # changed list means the ref has a changed ipn (changed in next revision)
+        unchanged = {}
+
+        # iterating through this dictionary's keys, if the key is in the other dictionary: check if this value is the same, if not the same, add to changed list
+        for key in thisDict.keys():
+            if key in otherDict.keys() and thisDict[key] == otherDict[key]:
+                unchanged.update({key: thisDict[key]})
+            else:
+                continue
+            
+        return unchanged
+    
+    def removedInNext(self, other):
+        thisDict = self
+        thisDictKeys = thisDict.keys()
+        otherDict = other
+        otherDictKeys = otherDict.keys()
+        
+        # removed list means the ref and ipn are in list1, but not the other list (removed in next revision); iterate through ref of thisDict, then compare with the ref of otherDict?
+        removed = {}
+        # iterating through this dictionary's keys, if the key is not in the other dictionary, then it was removed in the next revision
+        for key in thisDict.keys():
+            if key not in otherDictKeys:
+                removed.update({key: thisDict[key]})
+        return removed
+        
+    def addedInNext(self, other):
+        thisDict = self
+        otherDict = other
+
+        # added dictionary means a new ref and ipn are in list2, but not in the first list (added in next revision)
+        added = {}
+
+        # iterating through this dictionary's keys, if the key is in the other dictionary: check if this value is the same
+        for key in otherDict.keys():
+            if key not in thisDict.keys():
+                    #added[key] = otherDict[key]
+                added.update({key: otherDict[key]})
+
+        return added
+
+    def changedInNext(self, other):
+        thisDict = self
+        otherDict = other
+
+         # changed list means the ref has a changed ipn (changed in next revision)
+        changed = []
+
+        # iterating through this dictionary's keys, if the key is in the other dictionary: check if this value is the same, if not the same, add to changed list
+        for key in thisDict.keys():
+            if key in otherDict.keys() and thisDict[key] != otherDict[key]:
+                tempFromDict = {}
+                tempToDict = {}
+                tempFromDict.update({key: thisDict[key]})
+                tempToDict.update({key: otherDict[key]})
+                changed.append([tempFromDict, tempToDict])
+            else:
+                continue
+            
+        return changed
+    
+    def printChangedInNext(dict):
+        tempString = ""
+        tempString += "\nFrom:\t\t\tTo:"
+        tempString += "\nREF\tIPN\t\tREF\tIPN\n"
+        for i in dict:
+            for j in i:
+                for key, value in j.items():
+                    tempString += str(key) + "\t" + str(value) + "\t\t"
+            tempString += "\n"
+        
+        return tempString
+
+
+if __name__ == "__main__":
+    printBanner()
+    print("Files MUST be within the same folder and include '.prg'")
+    
+    file1Input = input("Enter Old Revision: ")
+    while path.isfile(file1Input) == False:
+        file1Input = input("Please re-enter Old Revision:")
+    else:
+        file2Input = input("Enter New Revision: ") 
+        while path.isfile(file2Input) == False:
+            file2Input = input("Please re-enter New Revision:")
+
+    refAndIpnDict1 = refAndIpn.collectRefAndIpn(file1Input)
+    refAndIpnDict2 = refAndIpn.collectRefAndIpn(file2Input)
+    adjustedInNextRevision = refAndIpn.unchangedInNext(refAndIpnDict1, refAndIpnDict2)
+    removedInNextRevision = refAndIpn.removedInNext(refAndIpnDict1, refAndIpnDict2)
+    addedInNextRevision = refAndIpn.addedInNext(refAndIpnDict1, refAndIpnDict2)
+    changedInNextRevision = refAndIpn.changedInNext(refAndIpnDict1, refAndIpnDict2)
+
+    # Creating a text file with name "revDifferenceRaw", which is the output of the terminal
+    file3Output = str((file1Input) + " compared to " + (file2Input))
+    with open(file3Output, 'w') as newFile:
+        now = datetime.now()
+        dt_string = now.strftime("%b-%d-%Y %H:%M:%S")
+        newFile.write("Date Created: " + repr(dt_string))
+
+        if refAndIpn.checkSame(refAndIpnDict1, refAndIpnDict2):
+            newFile.write("\n")
+            newFile.write("NO CHANGES IN NEXT REVISION")
+        else:
+            #print("Unchanged In Next Revision:")
+            #printDict(unchangedInNextRevision)
+
+            newFile.write("\n")
+            newFile.write("Rev1: " + repr(file1Input) + "\tLines: " + repr(len(refAndIpnDict1)))
+            newFile.write("\nRev1: " + repr(file2Input) + "\tLines: " + repr(len(refAndIpnDict2)))
+
+            newFile.write("\n\nCHANGES:")
+            if len(removedInNextRevision) > 0:
+                newFile.write("\n\nRemoved " + repr(len(removedInNextRevision)) + " Items in Next Revision (Removed from "+ repr(file1Input) + "): ")
+                tempString = printDict(removedInNextRevision)
+                newFile.write(tempString)
+
+            if len(addedInNextRevision) > 0:
+                newFile.write("\nAdded " + repr(len(removedInNextRevision)) + " Items in Next Revision (Added to "+ repr(file2Input) + "): ")
+                tempString = printDict(removedInNextRevision)
+                newFile.write(tempString)
+
+            if len(unchangedInNextRevision) > 0:
+                newFile.write("\nChanged " + repr(len(changedInNextRevision)) + " Items in Next Revision (Changed from " + repr(file1Input) + " to " + repr(file2Input) + "): ")
+                tempString = refAndIpn.printChangedInNext(changedInNextRevision)
+                newFile.write(tempString)
+
+            newFile.close()
+            # this keeps the .exe open after user inputs, and lets user know if new file has been created.
+            exitRequest = ""
+            if path.isfile(file3Output):
+                print("A new file: '" + file3Output + "' has succesfully been created.")
+                exitRequest = input("")
+            else:
+                print("No new file has been created. Format of input files are not compatible or have changed.")
+                exitRequest = input("")
+    
---
+++ RevisionCompareTool/RevisionCompareToolV1.1/test1.prg compared to test2.prg
@@ -0,0 +1,11 @@
+Date Created: 'Aug-21-2023 17:34:21'
+Rev1: 'test1.prg'	Lines: 335
+Rev1: 'test2.prg'	Lines: 336
+
+CHANGES:
+Added 1 Items in Next Revision (Added to 'test2.prg'): 
+REF	IPN
+
+Changed 0 Items in Next Revision (Changed from 'test1.prg' to 'test2.prg'): 
+From:			To:
+REF	IPN		REF	IPN
